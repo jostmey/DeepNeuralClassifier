e4bf0f93dacb8c897c0f2d02c83ba28ce2f8589c
Jared Ostmeyer <jostmey@users.noreply.github.com>
2015-11-24 19:05:04 -0600

    Update train.jl

--- train.jl
+++ train.jl
@@ -151,9 +151,9 @@
 			# Backpropagation for computing the gradients.
 			#
 			e5 = z-y5
-			e4 = (W45*e5).*(dsoftplus(y4).*r4)
-			e3 = (W34*e4).*(dsoftplus(y3).*r3)
-			e2 = (W23*e3).*(dsoftplus(y2).*r2)
+			e4 = (W45*e5).*dsoftplus(y4).*r4
+			e3 = (W34*e4).*dsoftplus(y3).*r3
+			e2 = (W23*e3).*dsoftplus(y2).*r2
 			e1 = (W12*e2).*dsoftmax(y1)
 
 			# Update change in parameters from this minibatch.
